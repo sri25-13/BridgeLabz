686e830421b052e784b3d258a53bccf80642b546
sri25-13 <sriharshinirao25@gmail.com>
2020-02-03 15:32:22 +0530

    [harshini]add .Implementation of gambler shellscript program

---
+++ gambler.sh
@@ -0,0 +1,30 @@
+echo "enter the stake:"
+read stake
+echo "enter the goal:"
+read goal
+echo "enter how many times  u wanna trial:"
+read trail
+bets=1
+wins=1
+for (( t=1; t<=$trail; t++ ))
+do
+ cash=$stake
+  while [[ $cash > 0 && $cash < $goal ]]
+  do
+    bets=$(( $bets + 1 ))
+     if [[ $(( 1 + $RANDOM % 2 )) -lt 5 ]]
+     then
+       cash=$cash++
+     else
+       cash=$$cash--
+     fi
+done
+if [ $cash -eq $goal ]
+then
+	wins=$wins++
+ fi
+done
+winper=100*$(($trials/$wins))
+#lossper=`expr 100 - $winper`
+echo "win percentage is $winper"
+#echo "loss percentage is $lossper"
