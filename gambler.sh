f163cf4734c49806b5bded51b10b6663a4301a7d
sri25-13 <sriharshinirao25@gmail.com>
2020-02-03 19:16:20 +0530

    [harshini]add .Implementation of gambler shellscript program

--- gambler.sh
+++ gambler.sh
@@ -3,6 +3,7 @@ read stake
 echo "enter the goal:"
 read goal
 echo "enter how many times  u wanna trial:"
+<<<<<<< HEAD
 read trail
 bets=1
 wins=1
@@ -28,3 +29,28 @@ winper=$(echo "$trials" "$wins" | awk '{print 100*$(($trials/$wins))}')
 lossper=`expr 100 - $winper`
 echo "win percentage is $winper"
 echo "loss percentage is $lossper"
+=======
+read trials
+bets=0
+wins=0
+for (( t=0; t<$trials; t++ ))
+do
+  while [[ $stake > 0 && $stake < $goal ]]
+  do
+     if [[ cash -lt 100 ]]
+     then
+       stake=$(($stake+1))
+     else
+       stake=$(($stake-1))
+     fi
+done
+if [ $stake == $goal ]
+then
+	wins=$(($wins+1))
+ fi
+done
+echo $wins "wins of" $trials
+echo "win percentage is =" $(($((100*$wins))/$trials))
+#echo "loss percentage is $losspe"
+
+>>>>>>> [harshini]add .Implementation of gambler shellscript program
