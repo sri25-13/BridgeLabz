431b11b14642d48edcfd619b900cd4136d7339d6
sri25-13 <sriharshinirao25@gmail.com>
2020-02-04 10:40:48 +0530

    [harshini]add .Implementation of  stopwatch shellscript program

--- stopwatch.sh
+++ stopwatch.sh
@@ -2,5 +2,5 @@ echo -n "enter start time:"
 read start
 echo -n "enter end time:"
 read end
-echo "elapsed time :"
+echo -n  "elapsed time in seconds :"
 echo $((end - start)) | awk '{print int($1%60)}'
