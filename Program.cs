9bb282bb9a88a0cd349aa561caeeb3b1f2f551c8
sri25-13 <sriharshinirao25@gmail.com>
2020-02-05 10:00:04 +0530

    [harshini]Add . Implementation of C-sharp Functional Programs

--- Program.cs
+++ Program.cs
@@ -7,13 +7,19 @@ namespace FunctionalPrograms
         static void Main(string[] args)
         {
 
-            Console.WriteLine("1.Replace");
+            Console.WriteLine("1.Replace\n2.powerof2\n3.PrimeFactors");
             Console.WriteLine("enter an option to be executed");
             int option = Utility.readInt();
             switch(option)
             {
                 case 1:
                     Replace.ReplaceString();
+                    break;
+                case 2:
+                    Powerof2.Power();
+                    break;
+                case 3:
+                    PrimeFactors.Prime();
                     break;
             }
 
