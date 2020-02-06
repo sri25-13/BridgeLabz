69334abb3e5f947f0004b2e4381e58e1af6bb540
sri25-13 <sriharshinirao25@gmail.com>
2020-02-06 12:32:30 +0530

    [harshini]Add . Implementation of C-sharp Functional Programs

---
+++ Gambler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalPrograms
+{
+    class Gambler
+    {
+        public static void Gambling()
+        {
+            Console.WriteLine("Enter stake value");
+            int stake = Utility.readInt();
+            Console.WriteLine("Enter goal");
+            int goal = Utility.readInt();
+            Console.WriteLine("enter the number of trials");
+            int trials = Utility.readInt();
+
+            int wins = 0;
+            Random rnd = new Random();
+            for (int t = 1; t < trials; t++)
+            {
+                int cash = stake;
+                if (cash > 0 && cash < goal)
+                {
+
+                    if (rnd.NextDouble() < 0.5)
+                    {
+                        cash++;
+                        wins++;
+
+                    }
+                    else
+                    {
+                        cash--;
+                    }
+                }
+
+                if (cash >= goal)
+
+                {
+                    Console.WriteLine("goal reached");
+                }
+            }
+
+            Console.WriteLine(wins + "wins of" + trials);
+            double a = ((100.0 * wins) / trials);
+            Console.WriteLine("percentage of games won:" + a);
+            Console.WriteLine("percentage of games lost:" + (100 - a));
+
+
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace FunctionalPrograms
         static void Main(string[] args)
         {
 
-            Console.WriteLine("1.Replace\n2.powerof2\n3.PrimeFactors\n4.HarmonicNum\n5.LeapYear\n6.FlipCoin\n7.Distance\n8.Quadratic\n9.WindChill\n10.SumofTrip\n11.twodiArray");
+            Console.WriteLine("1.Replace\n2.powerof2\n3.PrimeFactors\n4.HarmonicNum\n5.LeapYear\n6.FlipCoin\n7.Distance\n8.Quadratic\n9.WindChill\n10.SumofTrip\n11.twodiArray\n12.Gambler\n13.Coupon\n14.Stopwatch");
             Console.WriteLine("enter an option to be executed");
             int option = Utility.readInt();
             switch (option)
@@ -45,6 +45,16 @@ namespace FunctionalPrograms
                 case 11:
                     twodiArray.array2d();
                     break;
+                case 12:
+                    Gambler.Gambling();
+                    break;
+                case 13:
+                    Coupon.CouponNumber();
+                    break;
+                case 14:
+                    Stopwatch1.Stopwatch();
+                    break;
+
             }
             }
 
