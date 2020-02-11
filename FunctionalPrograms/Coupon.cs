7ad6eddc5c65bb61cc73e582e63408d8298de5c2
sri25-13 <sriharshinirao25@gmail.com>
2020-02-11 16:39:47 +0530

    [harshini]Refactor . Modified Functional Programs

---
+++ FunctionalPrograms/Coupon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalPrograms
+{
+    class Coupon
+    {
+        public static void CouponNumber()
+        {
+            Console.WriteLine("enter the coupon no length u need:");
+            int n= Utility.ReadInt();
+            Console.WriteLine("enter how many values u need:");
+            int n1 = Utility.ReadInt();
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[n];
+
+            Random rnd = new Random();
+            for (int j = 0; j < n1; j++)
+            {
+                for (int i = 0; i < stringChars.Length; i++)
+                {
+                    stringChars[i] = chars[rnd.Next(chars.Length)];
+                }
+
+
+               
+
+                Console.WriteLine(stringChars);
+
+            }
+        }
+    }
+}
