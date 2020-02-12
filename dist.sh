8e498abb94d7b5ced3751bde97a87920bbe888cc
sri25-13 <sriharshinirao25@gmail.com>
2020-02-12 10:54:10 +0530

    [harshini]Refactor . deleted duplicate Programs

--- dist.sh
+++
@@ -1,9 +0,0 @@
-echo -n "enter the value:"
-read x
-echo -n "enter another value:"
-read y
-a=`expr $x \* $x`
-b=`expr $y \* $y`
-c=`expr $a + $b`
-tot=`echo - | awk '{print sqrt('$c')}'`
-echo $tot
