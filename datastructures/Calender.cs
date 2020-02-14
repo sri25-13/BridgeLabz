7ab4bd63166fdb4f35e9d106001591c2b4500825
sri25-13 <sriharshinirao25@gmail.com>
2020-02-14 19:28:39 +0530

    [harshini]Add . Implementation of datastructure programs

--- datastructures/Calender.cs
+++ datastructures/Calender.cs
@@ -18,8 +18,10 @@ namespace DataStructures
        
         public static Boolean IsLeapYear(int year)
         {
-            if ((year % 4 == 0) && (year % 100 != 0)) return true;
-            if (year % 400 == 0) return true;
+            if ((year % 4 == 0) && (year % 100 != 0)) 
+                return true;
+            if (year % 400 == 0) 
+                return true;
             return false;
         }
 
