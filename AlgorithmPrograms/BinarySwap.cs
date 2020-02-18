7a2ed540411a790a2e2508e840bd34ea8300b9e8
sri25-13 <sriharshinirao25@gmail.com>
2020-02-18 10:02:25 +0530

    [harshini]Refactor . modify of algorithm programs

--- AlgorithmPrograms/BinarySwap.cs
+++ AlgorithmPrograms/BinarySwap.cs
@@ -32,15 +32,15 @@ namespace AlgorithmPrograms
         /// <param name="n">The n.</param>
         public static void BinaryConversion(int n)
         {
-            string str = "";
+            string s = "";
             while (n > 0)
             {
                 int num = n % 2;
                 n /= 2;
-                str = num + str;
+                s = num + s;
             }
 
-            Console.WriteLine(str);
+            Console.WriteLine(s);
         }
     }
 }
