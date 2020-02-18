97ee48c34fdb1295ee5e2908e8cb0c6920cdf5f5
sri25-13 <sriharshinirao25@gmail.com>
2020-02-18 19:01:00 +0530

    [harshini]Refactor . modify of Functional programs

---
+++ FunctionalPrograms/Powerof2.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=Powerof2.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="sriharshini"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace FunctionalPrograms
+{
+    using System;
+
+    /// <summary>
+    /// class for Powersoftwo
+    /// </summary>
+    public class Powerof2
+    {
+        /// <summary>
+        /// Power this instance.
+        /// </summary>
+        public static void Power()
+        {
+            Console.WriteLine("enter the number:");
+            int n = Utility.ReadInt();
+            int power = 1;
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine("2^" + i + "=" + power);
+
+                power = power * 2;
+            }
+        }
+    }
+}
