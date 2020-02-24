554f584c7aac6b965743f876c40c8bac7c221bb0
sri25-13 <sriharshinirao25@gmail.com>
2020-02-24 15:51:29 +0530

    [harshini]added. implementation of objectoriented programs

--- objectorientedprograms/Program.cs
+++ objectorientedprograms/Program.cs
@@ -6,13 +6,16 @@ namespace ObjectOrientedPrograms
         static void Main(string[] args)
         {
 
-            Console.WriteLine("1.InventoryManagement");
+            Console.WriteLine("1.InventoryManagement\n2.DeckOfCards");
             Console.WriteLine("enter an option to be executed");
             int option = Utility.ReadInt();
             switch(option)
             {
                 case 1:
                     InventoryManagement.Test();
+                    break;
+                case 2:
+                    DeckOfCards.DistributeCards();
                     break;
             }
         }
