37b0ef3bda375a94abde172f5e68246075720136
sri25-13 <sriharshinirao25@gmail.com>
2020-02-29 10:06:03 +0530

    [harshini]added. implementation of objectoriented programs

---
+++ objectorientedprograms/Delegates/Delegates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static ObjectOrientedPrograms.Delegates.Addition;
+
+namespace ObjectOrientedPrograms.Delegates
+{
+    class Delegates
+    {
+        public static void MultiCasting()
+        { 
+        Addition addition = new Addition();
+        Operation operation = new Operation(Addition.adding);
+            operation += operation.Muliplication();
+    }
+}
