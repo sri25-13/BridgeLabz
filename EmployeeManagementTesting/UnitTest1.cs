3bf572a31f2d857a2576734d2d61d1e5b86ea01d
sri25-13 <sriharshinirao25@gmail.com>
2020-03-23 11:30:05 +0530

    [harshini]refactor. Modify of employeemanagementsystem

--- EmployeeManagementTesting/UnitTest1.cs
+++ EmployeeManagementTesting/UnitTest1.cs
@@ -1,13 +1,24 @@
-using EmployeeManagementSystem.Manager;
-using EmployeeManagementSystem.Model;
-using NUnit.Framework;
-
-
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=UnitTest1.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="sriharshini"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace EmployeeManagementTesting
 {
+    using EmployeeManagementSystem.Manager;
+    using EmployeeManagementSystem.Model;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// test class
+    /// </summary>
     [TestFixture]
     public class Tests
     {
+        /// <summary>
+        /// Test the AddEmployee
+        /// </summary>
         [Test]
         public void AddEmployeeTest()
         {
@@ -17,27 +28,39 @@ namespace EmployeeManagementTesting
             employee.LastName = "rao";
             employee.Email = "harsh@gmail.com";
             employee.Password = "abc";
-            bool result = manager.AddEmployee(employee);
-            Assert.AreEqual(true,result);
+            string result = manager.AddEmployee(employee);
+            Assert.AreEqual("updated", result);
         }
+
+        /// <summary>
+        /// Test the DeleteEmployee
+        /// </summary>
         [Test]
         public void DeleteEmployeeTest()
         {
             EmployeeManager employeeManager = new EmployeeManager();
             Employee employee = new Employee();
-            employee.Id =12 ;
+            employee.Id = 12;
             bool result = employeeManager.DeleteEmployee(employee.Id);
-            Assert.AreEqual(true,result);
+            Assert.AreEqual(true, result);
         }
+
+        /// <summary>
+        /// Tests the LogIn
+        /// </summary>
         [Test]
         public void LoginTest()
         {
             EmployeeManager employeeManager = new EmployeeManager();
             string email = "harshini@gmail.com";
             string password = "harshinirao";
-            bool result = employeeManager.Login(email,password);
+            bool result = employeeManager.Login(email, password);
             Assert.AreEqual(true, result);
         }
+
+        /// <summary>
+        /// teste the updateEmployee
+        /// </summary>
         [Test]
         public void UpdateEmployee()
         {
@@ -49,7 +72,7 @@ namespace EmployeeManagementTesting
             employee.Email = "sri@gmail.com";
             employee.Password = "abcd";
             bool result = employeeManager.UpdateEmployee(employee);
-            Assert.AreEqual(true,result);
+            Assert.AreEqual(true, result);
         }
     }
 }
